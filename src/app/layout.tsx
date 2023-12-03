bcd3c4ed0e43014d07a8afad3acde38783898c01
Inman Costa <120146242+inmancosta@users.noreply.github.com>
2023-12-02 19:00:19 -0800

    initial file uploads

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './Navbar/Navbar'
+import Footer from './Footer';
+import SessionProvider from "./SessionProvider"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "Flowmazon",
+  description: "Making your wallet cry",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>
+        <Navbar />
+        <main className="p-4 max-w-7xl m-auto min-w[300px] ">
+        {children}
+        </main>
+        <Footer />
+        </SessionProvider>
+        </body>
+    </html>
+  )
+}
---
+++ src/app/search/page.tsx
@@ -0,0 +1,41 @@
+import ProductCard from "@/components/ProductCard";
+import prisma from "@/lib/db/prisma";
+import { Metadata } from "next";
+
+interface SearchPageProps {
+  searchParams: { query: string };
+}
+
+export function generateMetadata({
+  searchParams: { query },
+}: SearchPageProps): Metadata {
+    return {
+        title: `Search: ${query} - Flowmazon`
+    }
+}
+
+export default async function SearchPage({
+  searchParams: { query },
+}: SearchPageProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } }, //find if name contains what we searched for, regardless of capitalizaiton
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    orderBy: { id: "desc" },
+  });
+
+  if (products.length === 0) {
+    return <div className="text-center">No Products Found</div>;
+  }
+
+  return (
+    <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
+}
